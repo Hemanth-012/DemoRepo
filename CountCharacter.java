2642d35e8e8c56425a9160f674e24d803e7b2aa4
Hemanth-012 <vvce21cse0159@vvce.ac.in>
2024-07-10 15:58:01 +0530

    new message

---
+++ CountCharacter.java
@@ -0,0 +1,8 @@
+public class CountCharacter    
+{    
+    public static void main(String[] args) {    
+        String string = "The best of both worlds";    
+        System.out.println("hello");  
+         
+    }    
+}     
